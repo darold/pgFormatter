41c0b3a8a0e8744089eaf7ef7a25cab0231c5998
Darold Gilles <gilles@darold.net>
2015-04-16 11:56:08 +0200

    Prevent (....)::interval to be replaced by the anonymizer.

--- samples/ex4.sql
+++ samples/ex4.sql
@@ -1,3 +1,3 @@
 SELECT 1, 2, 10, 'depesz', 'hubert', 'depesz', 'hubert depesz', '1 2 3 4';
 
-SELECT tbl_lots.id, to_char ( tbl_lots.dt_crea, 'DD/MM/YYYY HH24:MI:SS' ) AS date_crea FROM tbl_lots WHERE tbl_lots.dt_crea > current_timestamp - interval '1 day' AND tbl_lots.type = 'SECRET';
+SELECT tbl_lots.id, to_char ( tbl_lots.dt_crea, 'DD/MM/YYYY HH24:MI:SS' ) AS date_crea FROM tbl_lots WHERE tbl_lots.dt_crea > current_timestamp - interval '1 day' AND  tbl_lots.dt_crea > current_timestamp - (dayrett || ' days')::interval AND tbl_lots.type = 'SECRET';
