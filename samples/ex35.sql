2fac61c9ebc9ffedbac51f6dfb226b87a0bbc252
Gilles Darold <gilles@darold.net>
2018-11-20 11:51:29 +0100

    Add DETACH,ATTACH,LIST,HASH,MODULUS and REMINDER to keywords list.

--- samples/ex35.sql
+++ samples/ex35.sql
@@ -1,15 +1,24 @@
 CREATE TABLE kbln (
     id integer NOT NULL,
     blank_series varchar(50) NOT NULL,
-    company_id varchar(8)) PARTITION BY
-    RANGE ( id);
+    company_id varchar(8)) partition by
+    range ( id);
 
-CREATE TABLE kbln_p0 PARTITION OF kbln FOR
-VALUES FROM ( MINVALUE) TO ( 500000) PARTITION BY HASH ( blank_series);
+create table kbln_p0 partition of kbln for
+values from ( minvalue) to ( 500000) partition by hash ( blank_series);
 
-CREATE TABLE kbln_p0_1 PARTITION OF kbln_p0 FOR
-VALUES WITH ( MODULUS 2, REMAINDER 0);
+create table kbln_p0_1 partition of kbln_p0 for
+values with ( modulus 2, remainder 0);
 
-CREATE TABLE kbln_p0_2 PARTITION OF kbln_p0 FOR
-VALUES WITH ( MODULUS 2, REMAINDER 1);
+create table kbln_p0_2 partition of kbln_p0 for
+values with ( modulus 2, remainder 1);
+
+alter table t1 detach partition t1_a;
+alter table t1 attach partition t1_a for values in (1, 2, 3);
+
+CREATE TABLE kbln (
+    id integer NOT NULL,
+    blank_series varchar(50) NOT NULL,
+    company_id varchar(8)) partition by
+    list ( id);
 
--- samples/expected/ex35.sql
+++ samples/expected/ex35.sql
@@ -5,13 +5,25 @@ CREATE TABLE kbln (
 )
 PARTITION BY RANGE (id);
 
-CREATE TABLE kbln_p0 OF kbln
+CREATE TABLE kbln_p0 of kbln
 FOR VALUES FROM (MINVALUE) TO (500000)
 PARTITION BY HASH (blank_series);
 
-CREATE TABLE kbln_p0_1 OF kbln_p0
-FOR VALUES WITH (MODULUS 2, REMAINDER 0);
+CREATE TABLE kbln_p0_1 of kbln_p0
+FOR VALUES WITH (MODULUS 2, remainder 0);
 
-CREATE TABLE kbln_p0_2 OF kbln_p0
-FOR VALUES WITH (MODULUS 2, REMAINDER 1);
+CREATE TABLE kbln_p0_2 of kbln_p0
+FOR VALUES WITH (MODULUS 2, remainder 1);
+
+ALTER TABLE t1 DETACH t1_a;
+
+ALTER TABLE t1 ATTACH t1_a
+FOR VALUES IN (1, 2, 3);
+
+CREATE TABLE kbln (
+    id integer NOT NULL,
+    blank_series varchar(50) NOT NULL,
+    company_id varchar(8)
+)
+PARTITION BY LIST (id);
 
