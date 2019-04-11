6c71d5cb0363f58272430c8aa6d16e73871bf33f
Gilles Darold <gilles@darold.net>
2019-04-11 17:55:15 +0200

    Update copyright year.

--- lib/pgFormatter/CGI.pm
+++ lib/pgFormatter/CGI.pm
@@ -508,7 +508,7 @@ Please report any bugs or feature requests to: https://github.com/darold/pgForma
 
 =head1 COPYRIGHT
 
-Copyright 2012-2018 Gilles Darold. All rights reserved.
+Copyright 2012-2019 Gilles Darold. All rights reserved.
 
 =head1 LICENSE
 
