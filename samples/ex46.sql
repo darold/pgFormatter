73e7b9098d01c12afb07bce5e45d94381102bd83
Darold Gilles <gilles@darold.net>
2019-04-21 15:58:06 +0200

    Add regression test for --wrap_after option.

---
+++ samples/ex46.sql
@@ -0,0 +1,3 @@
+SELECT a, b, c, d FROM t_1, t_2, t3 WHERE a = 10 AND b = 10 AND c = 10 AND d IN (1, 2, 3 , 4, 5 , 6, 7);
+
+SELECT a, b, c, d FROM t_1, t_2, (SELECT * FROM t6) as t3, t4 WHERE a = 10 AND b = 10 AND c = 10 AND d IN (1, 2, 3 , 4, 5 , 6, 7);
