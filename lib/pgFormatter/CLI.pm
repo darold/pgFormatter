fe954483a06ebaecbc324ef1dc9e6f58d219fd4a
Gilles Darold <gilles@darold.net>
2021-12-03 22:45:39 +0100

    Update ChangeLog and version to 5.2

--- lib/pgFormatter/CLI.pm
+++ lib/pgFormatter/CLI.pm
@@ -15,12 +15,12 @@ pgFormatter::CLI - Implementation of command line program to format SQL queries.
 
 =head1 VERSION
 
-Version 5.1
+Version 5.2
 
 =cut
 
 # Version of pgFormatter
-our $VERSION = '5.1';
+our $VERSION = '5.2';
 
 use autodie;
 use pgFormatter::Beautify;
