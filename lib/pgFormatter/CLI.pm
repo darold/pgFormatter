88581c4ed80ca161b5f456f22961179cb5db7590
Gilles Darold <gilles@darold.net>
2025-03-16 14:55:38 +0300

    Update copyright year

--- lib/pgFormatter/CLI.pm
+++ lib/pgFormatter/CLI.pm
@@ -526,7 +526,7 @@ Please report any bugs or feature requests to: https://github.com/darold/pgForma
 
 =head1 COPYRIGHT
 
-Copyright 2012-2024 Gilles Darold. All rights reserved.
+Copyright 2012-2025 Gilles Darold. All rights reserved.
 
 =head1 LICENSE
 
