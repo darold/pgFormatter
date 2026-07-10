3d3d3e1c1e03ca74d147bf85755f43b45080a9fc
david <david@aukes.com>
2026-07-10 18:55:23 +0200

    Simplified test
    Added the other table because it was my exact usecase

--- t/test-files/expected/ex81.sql
+++ t/test-files/expected/ex81.sql
@@ -4,11 +4,3 @@ CREATE TABLE example (
     email      varchar(100)             NOT NULL,                 -- third comment
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP -- last comment
 );
-
-CREATE TABLE queue_job (
-    id            uuid PRIMARY KEY          DEFAULT gen_random_uuid (),
-    parent_job_id uuid             REFERENCES queue_job (id) ON DELETE SET NULL,
-    status        queue_job_status NOT NULL DEFAULT 'queued',
-    payload       jsonb            NOT NULL DEFAULT '{}'::jsonb,
-    CONSTRAINT queue_job_status_check CHECK (status <> 'invalid')
-);
