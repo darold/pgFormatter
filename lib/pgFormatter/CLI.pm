2c3d9cca5aa32c2b6506df6c5940e7b8d360146a
edjubert <edouard.jubert@proton.me>
2026-09-07 15:05:37 +0200

    Add --join-on-indent option to indent multi-keyword join ON clauses
    Add a new option --join-on-indent that indents the ON clause
    continuation lines (AND/OR) of a multi-keyword join (e.g. LEFT OUTER
    JOIN) one level below the join keyword.
    
    By default the option is off and the existing behavior is preserved.
    
    Without the option (default):
        LEFT OUTER JOIN ab_frap appel ON appel.frap_tyap_code = tyap.tyap_code
        AND appel.frap_imme_no = tyap.tyap_imme_no
    
    With --join-on-indent:
        LEFT OUTER JOIN ab_frap appel ON appel.frap_tyap_code = tyap.tyap_code
            AND appel.frap_imme_no = tyap.tyap_imme_no

--- lib/pgFormatter/CLI.pm
+++ lib/pgFormatter/CLI.pm
@@ -131,6 +131,7 @@ sub beautify {
 	$args{'extra_keyword'}         = $self->{'cfg'}->{'extra-keyword'};
 	$args{'no_space_function'}     = $self->{'cfg'}->{'no-space-function'};
 	$args{'compact_clause_body'}   = $self->{'cfg'}->{'compact-clause-body'};
+	$args{'join_on_indent'}        = $self->{'cfg'}->{'join-on-indent'};
 	$args{'redundant_parenthesis'} = $self->{'cfg'}->{'redundant-parenthesis'};
 	$args{'vertical_align'}        = $self->{'cfg'}->{'vertical-align'};
 
@@ -333,6 +334,9 @@ Options:
     --compact-clause-body : keep the first element of a FROM, WHERE, SET, RETURNING,
                             HAVING or VALUES clause on the same line as the keyword,
                             and the body of a CASE ... THEN on the same line as THEN.
+    --join-on-indent      : indent the ON clause continuation lines (AND/OR) of a
+                            multi-keyword join (e.g. LEFT OUTER JOIN) one level
+                            below the join keyword.
     --redundant-parenthesis: do not remove redundant parenthesis in DML.
     --vertical-align      : vertically align CREATE TABLE column definitions and
                             trailing comments.
@@ -403,7 +407,7 @@ sub get_command_line_args {
 		'wrap-limit|w=i',  'wrap-after|W=i',
 		'inplace|i!',      'extra-function=s',
 		'extra-keyword=s', 'no-space-function!',
-		'compact-clause-body!', 'ident-case|I=i',
+		'compact-clause-body!', 'join-on-indent!', 'ident-case|I=i',
 		'redundant-parenthesis!', 'vertical-align!',
 	);
 
